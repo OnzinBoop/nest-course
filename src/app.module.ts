b8b0d33fc7212d57601d82282492b1adf182a388
JerremiahK <jeremiah@varias.io>
2021-05-25 15:39:28 +0200

    typeOrm module intro

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
+require('dotenv').config()
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [CoffeesModule, TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.DB_NAME,
+    autoLoadEntities: true,
+    synchronize: true
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
