40803bdc51d592582e64333bb80eb34bc12ba166
JerremiahK <jeremiah@varias.io>
2021-05-25 15:50:31 +0200

    creating a typeorm entity

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,9 +1,12 @@
 import { CoffeesService } from './coffees.service';
 import { CoffeesController } from './coffees.controller';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Coffee } from './Entities/coffee.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Coffee])],
   controllers: [CoffeesController],
   providers: [CoffeesService],
 })
-export class CoffeesModule {}
+export class CoffeesModule { }
